f75abdbeaa6a98e0ac9e14626bb6a408e4ad5027
Irene Aguas <hayreenfly@gmail.com>
2020-12-01 18:47:29 -0800

    Initial Setup for Vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App';
+import store from './store';
 
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+new Vue({ store, render: (h) => h(App) }).$mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex); // connects vue with vuex
+
+export default new Vuex.Store({
+  modules: {},
+});
